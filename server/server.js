b84671a930bf50fd5512057851676c1fdd124089
Luka <klancir.luka92@gmail.com>
2015-07-07 20:15:47 +0200

    package change

--- server/server.js
+++ server/server.js
@@ -1,18 +1,20 @@
 Meteor.startup(function(){
-  if(Meteor.users.find().fetch().length === 0){
-    var users = [
-      {name:'Customer Service',email:'cs@home.com',roles:['view-projects','view-customers']},
-      {name:'Admin Super User',email:'admin@home.com',roles:['admin']}
-    ];
-    _.each(users,function(userData){
-      var userid = Accounts.createUser({
-        email:userData.email,
-        password:'mytest1',
-        username:userData.email,
-        profile:{name:userData.name}
-      })
-      Meteor.users.update({_id:userid},{$set:{'emails.0.verified':true}});
-      Roles.addUsersToRoles(userid,userData.roles);
-    })
-  }
-})
+
+    console.log('Running server startup code...');
+
+  Accounts.onCreateUser(function (options, user) {
+    Roles.setRolesOnUserObj(user, ['admin','view-secrets']);
+
+    if (options.profile) {
+      // include the user profile
+      user.profile = options.profile
+    }
+
+    // other user object changes...
+    // ...
+
+    return user;
+  });
+
+  
+});
